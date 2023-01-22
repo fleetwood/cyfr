86f337366f7ccec1a7344e53b98a85b297d8f21d
Fleetwood <wizening@gmail.com>
2023-01-22 17:39:41 -0600

    Added /privacy and /tos

---
+++ cyfr-web/pages/tos.tsx
@@ -0,0 +1,45 @@
+import MainLayout from "../components/layouts/MainLayout";
+
+const tos = () => {
+  return (
+    <MainLayout pageTitle="Terms of Service" sectionTitle="Terms of Service">
+      <>
+        <div className="title">
+          <h1>TERMS AND CONDITIONS</h1>
+          <p>Last updated January 22, 2023</p>
+          <div>
+            <p>
+              These Terms and Conditions constitute a legally binding agreement
+              made between you, whether personally or on behalf of an entity
+              (“you”) and Fleetwood LLC ("<strong>Company</strong>", “
+              <strong>we</strong>”, “<strong>us</strong>”, or 
+              <strong>our</strong>”), concerning your access to and use of the
+              thisisatest.com website as well as any other media form, media
+              channel, mobile website or mobile application related, linked, or
+              otherwise connected thereto (collectively, the “Site”). We are
+              registered in Illinois, United States and have our registered
+              office at same.
+            </p>
+
+            <p>
+              You agree that by accessing the Site, you have read, understood,
+              and agree to be bound by all of these Terms and Conditions. IF YOU
+              DO NOT AGREE WITH ALL OF THESE TERMS AND CONDITIONS, THEN YOU ARE
+              EXPRESSLY PROHIBITED FROM USING THE SITE AND YOU MUST DISCONTINUE
+              USE IMMEDIATELY.
+            </p>
+
+            <p>
+              Supplemental terms and conditions or documents that may be posted
+              on the Site from time to time are hereby expressly incorporated
+              herein by reference. We reserve the right, in our sole discretion,
+              to make changes or modifications to these{" "}
+            </p>
+          </div>
+        </div>
+      </>
+    </MainLayout>
+  );
+};
+
+export default tos
