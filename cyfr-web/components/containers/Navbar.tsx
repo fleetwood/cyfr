50627e6f7a8299b1a557776c9ff3f1fe44b382e6
Fleetwood <wizening@gmail.com>
2023-02-06 16:21:55 -0600

    Added user navigation to Navbar

--- cyfr-web/components/containers/Navbar.tsx
+++ cyfr-web/components/containers/Navbar.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
-import useCyfrUser from "../../hooks/useCyfrUser";
-import { log } from "../../utils/log";
-import Avatar from "../ui/avatar";
-import { CyfrLogo, HouseIcon, UserIcon } from "../ui/icons";
-import ShrinkableIconLink from "../ui/shrinkableIconLink";
-import ShrinkableLink from "../ui/shrinkableLink";
+import { useEffect, useState } from 'react';
+import useCyfrUser from '../../hooks/useCyfrUser';
+import Avatar from '../ui/avatar';
+import { CyfrLogo, HouseIcon, UserIcon } from '../ui/icons';
+import ShrinkableIconLink from '../ui/shrinkableIconLink';
+import ShrinkableLink from '../ui/shrinkableLink';
+import Link from 'next/link';
 
 type NavbarProps = {
   className?: string;
@@ -55,14 +55,19 @@ const Navbar = ({
             className={iconClassName}
           />
           {cyfrUser ? (
-            <ShrinkableIconLink
-              href={"/account"}
-              // @ts-ignore
-              label={cyfrUser.name}
-              icon={<Avatar user={cyfrUser} sz='sm' link={false} />}
-              className={iconClassName}
-              dir="left"
-            />
+            <div className="dropdown dropdown-end text-secondary-content">
+            <label tabIndex={0} className="btn btn-ghost rounded-btn space-x-2">
+              <p>{cyfrUser.name}</p>
+              <Avatar user={cyfrUser} sz='sm' link={false} />
+            </label>
+            <ul tabIndex={0} className="menu dropdown-content p-2 drop-shadow-lg bg-secondary text-secondary-content bg-opacity-75 rounded-box mt-2 space-y-2 w-[400px]">
+              <li><Link href="#" className='hover:bg-opacity-100 hover:bg-secondary hover:drop-shadow-md'>Inbox</Link></li>
+              <li><Link href="#" className='hover:bg-opacity-100 hover:bg-secondary hover:drop-shadow-md'>Posts</Link></li>
+              <li><Link href={`/gallery/user/${cyfrUser.id}`} className='hover:bg-opacity-100 hover:bg-secondary hover:drop-shadow-md'>Galleries</Link></li>
+              <li><Link href={`/user/${cyfrUser.id}`} className='hover:bg-opacity-100 hover:bg-secondary hover:drop-shadow-md'>Profile</Link></li>
+              <li><Link href={`/account`} className='hover:bg-opacity-100 hover:bg-secondary hover:drop-shadow-md'>Acccount</Link></li>
+            </ul>
+          </div>
           ) : (
             <ShrinkableIconLink
               href={"/login"}
