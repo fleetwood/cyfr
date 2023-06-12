7f812d999c45cc5545b3cf418806ac810571a172
Fleetwood <wizening@gmail.com>
2023-06-12 18:45:30 -0500

    Added MaterialAccordion

---
+++ components/ui/accordion.tsx
@@ -0,0 +1,27 @@
+import {
+    Accordion,
+    AccordionBody,
+    AccordionHeader,
+} from "@material-tailwind/react";
+import { ReactFragment, ReactNode, useState } from "react";
+
+const MaterialAccordion = (props: { children: any[] }) => {
+  const [open, setOpen] = useState(0)
+  const handleOpen = (value:number) => setOpen(open === value ? -1 : value)
+
+  const items = Array.isArray(props.children) ? props.children : [...props.children]
+ 
+  return <>
+    {items.map((c,idx) => (
+        <Accordion open={open === idx} nonce={undefined} onResize={undefined} onResizeCapture={undefined}>
+            <AccordionHeader className={c.props.children[0].props.className??''} onClick={() => handleOpen(idx)} nonce={undefined} onResize={undefined} onResizeCapture={undefined}>
+                {c.props.children[0]}
+            </AccordionHeader>
+            <AccordionBody className={c.props.children[1].props.className??''} >
+                {c.props.children[1]}
+            </AccordionBody>
+        </Accordion>
+    ))}
+    </>
+}
+export default MaterialAccordion
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react"
+import { Fragment, useState } from "react"
 import AdminLayout from "../components/layouts/AdminLayout"
 import useDebug from "../hooks/useDebug"
+import MaterialAccordion from "components/ui/accordion"
 
 const { debug, info, fileMethod } = useDebug("test")
 
@@ -10,9 +11,50 @@ const Test = ({}) => {
   const steps = ['Bio', 'Appearance', 'Backstory', 'Thumbnail', 'Done']
   const [currentStep, setCurrentStep] = useState(0)
   const getLeeft = steps.map((s,i) => { return i === 0 ? `left-0` : `-left-[${(i*w)}px]`})[currentStep]
+  
+  const [disclosureState, setDisclosureState] = useState(-1)
+
+  const disclose = (i:number) => setDisclosureState((c) => i === c ? -1 : i)
 
   return (
     <AdminLayout sectionTitle="InlineTextArea">
+      <MaterialAccordion>
+        <>
+          <header>
+            What is Material Tailwind?
+          </header>
+          <body>
+            It is a thing.
+          </body>
+        </>
+        <>
+          <header>
+            What is not Material Tailwind?
+          </header>
+          <body>
+            It is not not a thing.
+          </body>
+        </>
+      </MaterialAccordion>
+
+      <MaterialAccordion>
+        <>
+          <header className="h4 text-primary">
+            What is This second Accordion?
+          </header>
+          <body className="bg-primary text-primary-content">
+            It is a second thing.
+          </body>
+        </>
+        <>
+          <header>
+            What is not this Second Accordion?
+          </header>
+          <body>
+            It is not not another thing.
+          </body>
+        </>
+      </MaterialAccordion>
       <div>
         <ul className="steps steps-vertical lg:steps-horizontal bg-base-100">
           {steps.map((step, idx) => (
