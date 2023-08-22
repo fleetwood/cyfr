24269cc69de0ab4064ef272d6969ff3660c9972c
Fleetwood <wizening@gmail.com>
2023-08-22 16:03:54 -0500

    Added pr-gen to build. Fixed GalleryCreatePage

--- pages/gallery/create.tsx
+++ pages/gallery/create.tsx
@@ -1,9 +1,11 @@
 import GalleryCreateModal from "../../components/containers/Gallery/GalleryCreateModal"
 import StaticLayout from "../../components/layouts/StaticLayout"
 
-const GalleryCreatePage = ({}) => 
+const GalleryCreatePage = (_props: any) => {
+    return (
     <StaticLayout sectionTitle="Create a Gallery!" >
         <GalleryCreateModal limit={25} />
     </StaticLayout>
+)}
 
 export default GalleryCreatePage
