ed285586adc65e76d21aad25fbfdf8a7a0d542f8
Fleetwood <wizening@gmail.com>
2023-08-23 22:44:47 -0500

    correcting hard-coded host in apiUrl

--- utils/api.ts
+++ utils/api.ts
@@ -2,7 +2,7 @@ import { __host__, __port__ } from "./constants"
 import axios from "axios"
 
 export const apiUrl = (url: string) =>
-  `http://${__host__}:${__port__}/api/${url}`
+  `/api/${url}`
 
 const postData = (body: any) => {
   return {
